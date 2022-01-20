125a9a191183d633f2f23b79dc1e50c3ba884250
birolisik <birol.isik@hotmail.com.tr>
2022-01-20 06:57:58 +0300

    update our menu-items to navigate and pathname

--- src/App.js
+++ src/App.js
@@ -1,10 +1,45 @@
+import react from 'react';
 import './App.css';
+import {Route, Routes} from 'react-router-dom';
 import HomePage from './pages/homepage/homepage.components';
+
+
+
+const HatsPage =props=>{
+  console.log(props);
+  return(
+    <div>
+<h1>HATS PAGE </h1>
+</div>
+  );
+
+};
+
+
+const TopicsList =()=>(
+  <div>
+  <h1>TOPIC LIST PAGE</h1>
+  </div>
+  );
+
+  const TopicDetail =(props)=>{
+  console.log(props);
+  return(
+    <div>
+    <h1>TOPIC DETAIL PAGE</h1>
+    </div>
+    );
+  };
 
 function App() {
   return (
-    <div >
-      <HomePage/>
+    <div className='App'>
+    <Routes>
+     <Route  path='/' element={<HomePage/>}/>
+     <Route exact path='/hats' element={<HatsPage/>}/>
+     <Route exact path='/topics' element={<TopicsList/>}/>
+     <Route exact path='/topics/:topicId' element={<TopicDetail/>}/>
+     </Routes>
     </div>
   );
 }
--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,14 +1,18 @@
 import React from "react";
+import {useNavigate,useLocation } from 'react-router-dom';
 import "./menu-item.styles.scss"
 
-const MenuItem =({title, imageUrl, size})=>(
-    <div className={`${size} menu-item`}>
+const MenuItem =({title, imageUrl, size,history, linkUrl,match})=>{
+    const navigate = useNavigate();
+    let { pathname } = useLocation();
+    return(
+    <div className={`${size} menu-item`} onClick={()=>navigate(`${pathname}${linkUrl}`)}>
     <div className="background-image" style={{backgroundImage:`url(${imageUrl})`}}/>
     <div className="content">
         <h1 className="title">{title.toUpperCase()}</h1>
         <span className="subtitle">Shop Now</span>
     </div>
 </div>
-)
+)};
 
 export default MenuItem;
